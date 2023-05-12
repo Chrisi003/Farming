1258720fcdd883672aaa7a032eba8722de19cb84
Chrisi003 <christopher.baran@student.ukf.sk>
2023-05-12 19:49:17 +0200

    aa

--- partials/footer.php
+++ partials/footer.php
@@ -85,3 +85,11 @@
             </div>
          </div>
       </footer>
+      <script src="js/jquery.min.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/jquery-3.0.0.min.js"></script>
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="js/custom.js"></script>
+      </body>
+</html>
