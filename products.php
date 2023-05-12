194c7699df59850843d50079dbe685c3fc2fe817
Chrisi003 <christopher.baran@student.ukf.sk>
2023-05-12 19:45:22 +0200

    a

---
+++ products.php
@@ -0,0 +1,64 @@
+<?php
+    include('partials/header.php');
+  ?>
+      <!-- product  section -->
+      <div class="product">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-6">
+                  <div class="titlepage">
+                     <h2>Our Product</h2>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="container-fluid">
+            <div class="row">
+               <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 padding_left0">
+                  <div class="product_box">
+                     <figure><img src="images/product1.jpg" alt="#"/></figure>
+                     <h3 class="black">vegetable</h3>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12">
+                  <div class="product_box">
+                     <figure><img src="images/product2.jpg" alt="#"/></figure>
+                     <h3 >weat</h3>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 padding_right0">
+                  <div class="product_box">
+                     <figure><img src="images/product3.jpg" alt="#"/></figure>
+                     <h3>fruit</h3>
+                  </div>
+               </div>
+               <div class="col-xl-7 col-lg-7 col-md-7 col-sm-12 padding_left0">
+                  <div class="product_box">
+                     <figure><img src="images/product4.jpg" alt="#"/></figure>
+                     <h3>sunflowere</h3>
+                  </div>
+               </div>
+               <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12 padding_right0">
+                  <div class="product_box">
+                     <figure><img src="images/product5.jpg" alt="#"/></figure>
+                     <h3>Livestock</h3>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- end product  section -->
+      <?php
+    include('partials/footer.php');
+      ?>
+      <!-- Javascript files-->
+      <script src="js/jquery.min.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/jquery-3.0.0.min.js"></script>
+      <!-- sidebar -->
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="js/custom.js"></script>
+   </body>
+</html>
+
